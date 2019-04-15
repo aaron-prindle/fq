f07139a4b87c0510a071eb9f5559aa9c4aaf27bf
Aaron Prindle <aprindle@google.com>
2019-04-15 11:37:56 -0700

    working fq impl w/ tests

---
+++ fq.go
@@ -0,0 +1,165 @@
+package fq
+
+import (
+	"math"
+	"sync"
+	"time"
+)
+
+const (
+	scaledOne uint64 = 1 << 16
+)
+
+// mods for our algo
+
+type Packet struct {
+	// request   http.Request
+	item      interface{}
+	virfinish uint64
+	size      uint64
+	queue     *Queue
+	//
+	key    uint64
+	weight uint8
+	seq    uint64
+}
+
+func (q *fqscheduler) chooseQueue(packet *Packet) *Queue {
+	for _, queue := range q.queues {
+		if packet.key == queue.key {
+			return queue
+		}
+	}
+	// fmt.Printf("packet w/ key: %v\n", packet.key)
+	panic("no matching queue for packet")
+}
+
+type Queue struct {
+	Packets       []*Packet
+	key           uint64
+	lastvirfinish uint64
+}
+
+func (q *Queue) enqueue(packet *Packet) {
+	q.Packets = append(q.Packets, packet)
+}
+
+func (q *Queue) dequeue() (*Packet, bool) {
+	if len(q.Packets) == 0 {
+		return nil, false
+	}
+	packet := q.Packets[0]
+	q.Packets = q.Packets[1:]
+	return packet, true
+}
+
+type fqscheduler struct {
+	lock   sync.Mutex
+	queues []*Queue
+	closed bool
+}
+
+func newfqscheduler(queues []*Queue) *fqscheduler {
+	fq := &fqscheduler{
+		queues: queues,
+	}
+
+	return fq
+}
+
+func (q *fqscheduler) enqueue(packet *Packet) {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+
+	queue := q.chooseQueue(packet)
+	// order of updatetime and enqueue changes things?
+	// q.updateTime(packet, queue)
+	// queue.enqueue(packet)
+	queue.enqueue(packet)
+	q.updateTime(packet, queue)
+
+}
+
+func max(a, b uint64) uint64 {
+	if a >= b {
+		return a
+	}
+	return b
+}
+
+func (q *fqscheduler) updateTime(packet *Packet, queue *Queue) {
+	// virStart is the virtual start of service
+	virStart := max(uint64(time.Now().UnixNano()), queue.lastvirfinish)
+	// adding multiplier dramatically increases test ratios
+	// packet.virfinish = packet.size + virStart
+	packet.virfinish = packet.size*(scaledOne) + virStart
+	queue.lastvirfinish = packet.virfinish
+}
+
+func (q *fqscheduler) dequeue() (*Packet, bool) {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+
+	queue := q.selectQueue()
+	packet, ok := queue.dequeue()
+	return packet, ok
+}
+
+func (q *fqscheduler) selectQueue() *Queue {
+	minvirfinish := uint64(math.MaxUint64)
+	minqueue := q.queues[0]
+	for _, queue := range q.queues {
+		if len(queue.Packets) != 0 && queue.Packets[0].virfinish < minvirfinish {
+			minvirfinish = queue.Packets[0].virfinish
+			minqueue = queue
+		}
+	}
+	return minqueue
+}
+
+func initQueues(n int, key uint64) []*Queue {
+	queues := []*Queue{}
+	for i := 0; i < n; i++ {
+		qkey := key
+		if key == 0 {
+			qkey = uint64(i)
+		}
+		queues = append(queues, &Queue{
+			Packets: []*Packet{},
+			key:     qkey,
+		})
+	}
+
+	return queues
+}
+
+// func echoHandler(w http.ResponseWriter, r *http.Request) {
+// 	if sleepTimeMillis, err := strconv.Atoi(r.Header.Get("sleep")); err == nil {
+// 		sleepTime := time.Millisecond * time.Duration(float64(sleepTimeMillis)*(1+rand.Float64()))
+// 		time.Sleep(sleepTime)
+// 	}
+// 	w.Write([]byte("loktar ogar"))
+// }
+
+// func main() {
+// 	runtime.GOMAXPROCS(2)
+// 	FQServer(echoHandler)
+// }
+
+// func FQServer(actualHandler http.HandlerFunc) {
+// 	http.HandleFunc("/", newHandler(initQueues(100, 1), actualHandler))
+// 	log.Fatal(http.ListenAndServe(":8080", nil))
+// }
+
+// func newHandler(allQueues []*Queue, delegateHandler http.HandlerFunc) http.HandlerFunc {
+// 	if len(allQueues) == 0 {
+// 		return delegateHandler
+// 	}
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		// w.WriteHeader(http.StatusConflict)
+// 		// fmt.Println("limitted!!")
+// 		// w.Write([]byte("limitted"))
+// 		// panic("not matching buckets")
+// 		delegateHandler(w, r)
+// 	}
+// }
---
+++ fq_test.go
@@ -0,0 +1,172 @@
+package fq
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"runtime"
+	"sync"
+	"testing"
+	"time"
+)
+
+type flowDesc struct {
+	// In
+	ftotal uint64 // Total units in flow
+	imin   uint64 // Min Packet size
+	imax   uint64 // Max Packet size
+	weight uint8  // Flow weight
+
+	// Out
+	idealPercent  float64
+	actualPercent float64
+}
+
+func genFlow(fq *fqscheduler, desc *flowDesc, key uint64, done_wg *sync.WaitGroup) {
+	for i, t := uint64(1), uint64(0); t < desc.ftotal; i++ {
+		//time.Sleep(time.Microsecond)
+		it := new(Packet)
+		it.key = key
+		if desc.imin == desc.imax {
+			it.size = desc.imax
+		} else {
+			it.size = desc.imin + uint64(rand.Int63n(int64(desc.imax-desc.imin)))
+		}
+		if t+it.size > desc.ftotal {
+			it.size = desc.ftotal - t
+		}
+		t += it.size
+		it.weight = desc.weight
+		it.seq = i
+		// new packet
+		fq.enqueue(it)
+	}
+	(*done_wg).Done()
+}
+
+func consumeQueue(fq *fqscheduler, descs []flowDesc) (float64, error) {
+	active := make(map[uint64]bool)
+	var total uint64
+	acnt := make(map[uint64]uint64)
+	cnt := make(map[uint64]uint64)
+	seqs := make(map[uint64]uint64)
+
+	var wsum uint64
+	for _, d := range descs {
+		wsum += uint64(d.weight + 1)
+	}
+
+	hasEntered := false
+	for hasEntered {
+		for i, ok := fq.dequeue(); ok; i, ok = fq.dequeue() {
+			hasEntered = true
+			time.Sleep(time.Microsecond) // Simulate constrained bandwidth
+			it := i
+			seq := seqs[it.key]
+			if seq+1 != it.seq {
+				return 0, fmt.Errorf("Packet for flow %d came out of queue out-of-order: expected %d, got %d", it.key, seq+1, it.seq)
+			}
+			seqs[it.key] = it.seq
+
+			if cnt[it.key] == 0 {
+				active[it.key] = true
+			}
+			cnt[it.key] += it.size
+
+			if len(active) == len(descs) {
+				acnt[it.key] += it.size
+				total += it.size
+			}
+
+			if cnt[it.key] == descs[it.key].ftotal {
+				delete(active, it.key)
+			}
+		}
+	}
+
+	var variance float64
+	for key := uint64(0); key < uint64(len(descs)); key++ {
+		descs[key].idealPercent = (((float64(total) * float64(descs[key].weight+1)) / float64(wsum)) / float64(total)) * 100
+		descs[key].actualPercent = (float64(acnt[key]) / float64(total)) * 100
+		x := descs[key].idealPercent - descs[key].actualPercent
+		x *= x
+		variance += x
+	}
+
+	stdDev := math.Sqrt(variance)
+	return stdDev, nil
+}
+
+func TestSingleFlow(t *testing.T) {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	queues := initQueues(1, 1)
+	fq := newfqscheduler(queues)
+
+	go func() {
+		for i := 1; i < 10000; i++ {
+			it := &Packet{}
+			it.key = 1
+			it.size = uint64(rand.Int63n(10) + 1)
+			it.weight = 1
+			it.seq = uint64(i)
+			fq.enqueue(it)
+		}
+	}()
+
+	var seq uint64
+	hasEntered := false
+	for hasEntered {
+		for it, ok := fq.dequeue(); ok; it, ok = fq.dequeue() {
+			if seq+1 != it.seq {
+				t.Fatalf("Packet came out of queue out-of-order: expected %d, got %d", seq+1, it.seq)
+			}
+			seq = it.seq
+		}
+	}
+}
+
+func TestUniformMultiFlow(t *testing.T) {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	queues := initQueues(1000, 0)
+	fq := newfqscheduler(queues)
+
+	var swg sync.WaitGroup
+	var wg sync.WaitGroup
+
+	var flows = []flowDesc{
+		{1000, 1, 1, 0, 0, 0},
+		{1000, 1, 1, 0, 0, 0},
+		{1000, 1, 1, 0, 0, 0},
+		{1000, 1, 1, 0, 0, 0},
+		{1000, 1, 1, 0, 0, 0},
+		{1000, 1, 1, 0, 0, 0},
+		{1000, 1, 1, 0, 0, 0},
+		{1000, 1, 1, 0, 0, 0},
+		{1000, 1, 1, 0, 0, 0},
+		{1000, 1, 1, 0, 0, 0},
+	}
+
+	swg.Add(1)
+	wg.Add(len(flows))
+	for n := 0; n < len(flows); n++ {
+		go genFlow(fq, &flows[n], uint64(n), &wg)
+	}
+
+	go func() {
+		wg.Wait()
+	}()
+	swg.Done()
+
+	stdDev, err := consumeQueue(fq, flows)
+
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	if stdDev > 0.1 {
+		for k, d := range flows {
+			t.Logf("For flow %d: Expected %v%%, got %v%%", k, d.idealPercent, d.actualPercent)
+		}
+		t.Fatalf("StdDev was expected to be < 0.1 but got %v", stdDev)
+	}
+}
